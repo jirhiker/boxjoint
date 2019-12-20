4281ebe2da8607667f678cdc04f344c6fb4c572a
jross <jirhiker@gmail.com>
2019-12-20 15:58:00 -0700

    update

--- boxjoint/boxjoint.ino
+++ boxjoint/boxjoint.ino
@@ -1,9 +1,18 @@
+
+#include <AccelStepper.h>
+
 #define PUL 52
 #define DIR 53
 #define EN 50
 
+
+//AccelStepper stepper=AccelStepper(1, PUL, DIR);
+
 char user_input;
 void setup() {
+  //stepper.setEnablePin(EN);
+  //stepper.setMaxSpeed(1000);
+  //stepper.setSpeed(50);
   // put your setup code here, to run once:
   pinMode(PUL, OUTPUT);
   pinMode(DIR, OUTPUT);
@@ -15,20 +24,33 @@ void loop() {
   // put your main code here, to run repeatedly:
   while(Serial.available()){
       user_input = Serial.read();
-      if (user_input =='1')
+      if (user_input =='f')
       {
         Serial.println("Doing F steps");
-        stepper(3*400, HIGH);
-      }else if (user_input =='2')
+        do_steps(3*400, HIGH);
+      }else if (user_input =='r')
       {
         Serial.println("Doing R steps");
-        stepper(3*400, LOW);
+        do_steps(3*400, LOW);
+        
+      }
+      else if (user_input=='d')
+      {
+        digitalWrite(EN, LOW);
+      }
+      else if (user_input=='3')
+      {
+        Serial.println("Doing run");
+        //stepper.runSpeed();
+        delay(5000);
+        //stepper.stop();
+        Serial.println("Run finished");
       }
   }
 }
 
-double pulse_width=200;
-void stepper(int steps, bool dir){
+double pulse_width=250;
+void do_steps(int steps, bool dir){
   digitalWrite(EN, HIGH);
   digitalWrite(DIR, dir);
   delayMicroseconds(5);
